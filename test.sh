e387381f4cbdcd46c8bcfe58225e89dd9ae59116
Ricardo Amaro <mail@ricardoamaro.com>
2014-02-10 17:24:45 -0500

    added the test.sh script for automating the work

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+NAME="${USER}/drupaltestbot"
+
+# if image ${USER}/drupal does not exist run:
+IMAGE=$(sudo docker images | grep "${NAME}")
+
+if [ "$IMAGE" == "" ]; then 
+  echo "Docker image ${NAME} needs to be created..."; 
+  sudo docker build -t ${NAME} . ;
+  else
+  echo "Docker image ${NAME} found!"; 
+fi
+
+CONTAINER=$(sudo docker run -i -d ${NAME} | tail -n1)
+echo ${CONTAINER}
+sleep 30
+sudo docker logs ${CONTAINER}
+
